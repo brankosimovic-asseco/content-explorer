4615a0ffc3060e3fd8ed543ef0bd28c20717605b
brankosimovic-asseco <branko.simovic@asseco-see.rs>
2021-05-07 16:59:22 +0200

    style refactor

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let baseUrl = 'http://demo.dbranch.asseco.rs';
 let contentUrl = new URL('/v1/content/reponame/', baseUrl);
 let paths = new Array('');
-let currentPage = 1, totalPages = 1;
+let currentPage = 1,
+  totalPages = 1;
 let selectedItem = null;
 let currentFolderId = -1;
 let token = `eyJhbGciOiJSUzI1NiIsImtpZCI6IkI1Q0MxMTdBRjdGNTNFNEY3RDg0NDZDMkE0M0VEOTY0NTBCNTc3NzBSUzI1NiIsInR5cCI6ImF0K2p3dCIsIng1dCI6InRjd1JldmYxUGs5OWhFYkNwRDdaWkZDMWQzQSJ9.eyJuYmYiOjE2MjAyMjY1MTMsImV4cCI6MTYyMDI1NTMxMywiaXNzIjoiaHR0cDovL2RlbW8uZGJyYW5jaC5hc3NlY28ucnMvdjEvYXV0aGVudGljYXRpb24iLCJhdWQiOlsiYnBtIiwiY29uZmlndXJhdGlvbiIsImNvbW1lbnRzIiwiY29yZS1pbnRlZ3JhdGlvbiIsIm9mZmVyIiwiY29udGVudCIsImRlY2lzaW9uIiwiZG9jdW1lbnQtY29tcG9zaXRpb24iLCJkaXJlY3RvcnkiXSwiY2xpZW50X2lkIjoic2hlbGwtdWkiLCJzdWIiOiI3MTNhYzM3My0zMzcyLTRkN2EtODUwNy00MjA1MTI2ZGNkMzgiLCJhdXRoX3RpbWUiOjE2MjAyMjY1MTMsImlkcCI6ImRlZmF1bHQiLCJyb2xlIjpbIk9wZXJhdGlvbiBvZmZpY2VyLDAwMDEiLCJCcmFuY2ggbWFuYWdlciwwMDAxIiwiUmVsYXRpb25zaGlwIG1hbmFnZXIiLCJVbmRlcndyaXRlciIsIkhlYWQgb2YgdW5kZXJ3cml0aW5nIiwiQWRtaW4sMDAwMSIsIkNyZWRpdCBBZG1pbiJdLCJtYWluX29yZ2FuaXphdGlvbl91bml0IjoiMDAwMSIsInVzZXJfdHlwZSI6IkFkbWluIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYnJhbmtvLnNpbW92aWMiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwiYnBtIiwiY29uZmlndXJhdGlvbiIsImNvbW1lbnRzIiwiY29yZS1pbnRlZ3JhdGlvbiIsIm9mZmVyIiwiY29udGVudCIsImRlY2lzaW9uIiwiZG9jdW1lbnQtY29tcG9zaXRpb24iLCJkaXJlY3RvcnkiXSwiYW1yIjpbImV4dGVybmFsIl19.ewMEhLjPwAHpUQyP8UxrQOPxC_ONxUPbsNqnXPFc9F205mz-AbKLgMKdUvr_LBHxa7OlmTCSBofE07T57g5m-fJRHmOpWk9fRTYgBCtLWcUr8raF5oG6nUq4p4MYlznPkY294xIcZqxXofOHtQyaCus5Dy5-PJuNYpvbwI7r5qqqpErSjyEPhhrTbPoBjyi1nQdU26HeAVPiafCZhuX_sFH3beXUW2VYUvIu-bNTWCc_A-bKBWayTvLCjVmnE6FHNB0N4l1q_QnxnllMGmVb0Jfg7N2glmkS_TSZcpzih3dl1k6nV0u6WBCt3sGRSIbHBW4aqEbwcRS65_KkSRmljQ`;
@@ -14,18 +15,17 @@ $(document).ready(() => {
 function generateBreadcrumbs() {
   let breadCrumbs = $('.breadcrumbz').first();
   breadCrumbs.empty();
-  paths
-    .forEach((path) => {
-      if(path === '') path = 'repo'; 
-      const breadCrumbElementString = `<div class="path">${path}</div>`;
-      const breadCrumbElement = $.parseHTML(breadCrumbElementString);
+  paths.forEach((path) => {
+    if (path === '') path = 'repo';
+    const breadCrumbElementString = `<div class="path">${path}</div>`;
+    const breadCrumbElement = $.parseHTML(breadCrumbElementString);
 
-      $(breadCrumbElement).on('click', (e) => {
-        const folderName = $(e.currentTarget).text();
-        getFolderItems(folderName, true);
-      });
-      breadCrumbs.append(breadCrumbElement);
+    $(breadCrumbElement).on('click', (e) => {
+      const folderName = $(e.currentTarget).text();
+      getFolderItems(folderName, true);
     });
+    breadCrumbs.append(breadCrumbElement);
+  });
 }
 
 /**
@@ -52,30 +52,27 @@ function generateCurrentFolderItems(data) {
   data.items.forEach((item) => {
     let folderIconElementString = '';
 
-    const downloadIconString =
-      item.kind === 'folder'
-        ? ''
-        : `<div class="download-button"><img src="assets/download.svg" width="28" alt=""></div>`;
+    const optionsButtonString = `<div tabindex="0" class="menu-button">
+      <img src="assets/more.svg" width="14" alt="">
+    </div>`;
 
     switch (item.kind) {
       case 'folder':
         folderIconElementString = `<img src="assets/folder.svg" width="48" alt="">`;
         break;
       case 'document':
-        folderIconElementString = `<div><img src="${getDocumentType(
-          item
-        )}" width="48" alt=""></div>${downloadIconString}`;
+        folderIconElementString = `<div><img src="${getDocumentType(item)}" width="48" alt=""></div>`;
         break;
       default:
         break;
     }
 
     // TODO: change this later
-    const name = item?.name?.length > 19 ? item.name.substring(0, 19) + '...' : item.name;
+    const name = item?.name?.length > 19 ? item.name.substring(0, 16) + '...' : item.name;
 
     const folderItemString = `
         <div class="item" tabindex="0">
-            ${folderIconElementString}   
+            ${folderIconElementString} ${optionsButtonString}  
             <div title="${item.name}" class="item-text">${name}</div>
         </div>`;
 
@@ -92,10 +89,11 @@ function generateCurrentFolderItems(data) {
           openInNewTab(contentUrl.href + paths.slice(1, paths.length).join('/') + '/' + name);
         });
     }
-    $(folderItemElement).on('click', (e) => {
-      // console.log(item);
-      selectedItem = item;
-    });
+    $(folderItemElement)
+      .children('.menu-button')
+      .on('click', (e) => {
+        console.log(item, 'menu item toggled!');
+      });
 
     $('.container').append(folderItemElement);
   });
@@ -124,7 +122,6 @@ async function deleteFolder(folderId) {
  * @param {boolean} levelChange Indicates if we are changing to another level
  */
 async function getFolderItems(folder, levelChange) {
-
   $('.loader').css('display', 'initial');
 
   let url = new URL('/v1/content/reponame/', baseUrl);
@@ -134,8 +131,7 @@ async function getFolderItems(folder, levelChange) {
   if (levelChange) currentPage = 1;
 
   if (typeof folder === 'string') {
-
-    if(!paths.includes(folder)) folder = '';
+    if (!paths.includes(folder)) folder = '';
     paths = paths.slice(0, paths.indexOf(folder) + 1);
     url.pathname += paths.join('/');
   } else {
@@ -143,7 +139,7 @@ async function getFolderItems(folder, levelChange) {
     url.pathname += paths.slice(1, paths.indexOf(folder.name) + 1).join('/');
   }
   url.searchParams.append('page-size', '40');
-  url.searchParams.append('page', currentPage.toString())
+  url.searchParams.append('page', currentPage.toString());
   const response = await fetch(url.href);
   const data = await response.json();
 
@@ -205,11 +201,6 @@ function getDocumentType(item) {
   let iconPath = '';
   if (item['media-type'].split('/')[0] === 'image') {
     iconPath = 'assets/image.svg';
-  } else if (item['media-type'].split('/')[1] === 'pdf') {
-    iconPath = 'assets/pdf-file.svg';
-  } else if(item['media-type'].split('/')[1] === 'vnd.openxmlformats-officedocument.wordprocessingml' ||
-            item['media-type'].split('/')[1] ===  'vnd.openxmlformats-officedocument.wordprocessingml.document') {
-    iconPath = 'assets/doc-file.svg';
   } else {
     iconPath = 'assets/doc.svg';
   }
@@ -255,14 +246,14 @@ function setEventListeners() {
     uploadDocumentToCurrentFolder(selectedFile, fp, fcn);
     $('.dialog').css('display', 'none');
     $('#overlay').css('display', 'none');
-  })
+  });
 
   $('#upload-field').change(() => {
     $('.dialog').css('display', 'initial');
     $('#overlay').css('display', 'initial');
   });
 
-  $('#close-upload').on('click',() => {
+  $('#close-upload').on('click', () => {
     $('.dialog').css('display', 'none');
     $('#overlay').css('display', 'none');
   });
